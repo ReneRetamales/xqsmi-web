3a3385815843f26e4689bf60163b627d3376a4cd
ReneRetamales <reneretamalespinto1@gmail.com>
2025-11-05 12:16:55 -0300

    0.6.9 | Pruebas de colores, cambios mínimos (solo pruebas)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
+// import "./globals.css";
+
+// export const metadata: Metadata = {
+//   title: "XqsMi | Sitio Web Oficial",
+//   description: "Sitio web oficial de XqsMi",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="es">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
+
+export const viewport: Viewport = {
+  themeColor: "#1A73E8",
+};
 
 export const metadata: Metadata = {
   title: "XqsMi | Sitio Web Oficial",
